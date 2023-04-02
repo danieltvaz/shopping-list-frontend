561612f0640b0182d1a50e9901ba18d6bab776fa
danieltvaz <danieltostes@live.com>
2023-04-02 14:56:57 -0300

    refactor: change base name

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,24 +4,27 @@ import ProtectedRoute from "../hocs/ProtectedRoute";
 import SignupPage from "../pages/signup";
 import { createBrowserRouter } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/lista-de-compras",
-    element: <LoginPage />,
-    index: true,
-  },
-  {
-    path: "/signup",
-    element: <SignupPage />,
-  },
-  {
-    path: "/products",
-    element: (
-      <ProtectedRoute>
-        <ProductListPage />
-      </ProtectedRoute>
-    ),
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <LoginPage />,
+      index: true,
+    },
+    {
+      path: "/signup",
+      element: <SignupPage />,
+    },
+    {
+      path: "/products",
+      element: (
+        <ProtectedRoute>
+          <ProductListPage />
+        </ProtectedRoute>
+      ),
+    },
+  ],
+  { basename: "/lista-de-compras" }
+);
 
 export default router;
