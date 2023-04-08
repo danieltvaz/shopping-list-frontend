26ce61d66ae49004aac287d405da3e9316801e71
danieltvaz <danieltostes@live.com>
2023-04-07 22:56:26 -0300

    refactor: abstract button

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -4,11 +4,19 @@ import { ComponentProps } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquarePlus } from "@fortawesome/free-regular-svg-icons";
 
-type AddButtonProps = ComponentProps<"button"> & { icon?: boolean; text: string };
+type ButtonProps = ComponentProps<"button"> & {
+  icon?: boolean;
+  text: string;
+  variant?: "normal" | "danger";
+  size?: string;
+};
 
-export default function AddButton({ icon, text, ...props }: AddButtonProps) {
+export default function Button({ icon, text, variant = "normal", size = "150px", ...props }: ButtonProps) {
   return (
-    <button className="addbutton__button" {...props}>
+    <button
+      className="addbutton__button"
+      {...props}
+      style={{ backgroundColor: variant === "normal" ? "#007bff" : "#a22607", width: size }}>
       {icon && <FontAwesomeIcon icon={faSquarePlus} color="#fff" />}
       {text}
     </button>
