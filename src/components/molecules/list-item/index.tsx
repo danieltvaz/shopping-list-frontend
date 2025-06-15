ef8e81c2be02f686a881edfeb40e30583bb26037
danieltvaz <danieltostes@live.com>
2025-06-15 18:55:21 -0300

    refactor: use new flex container component for reponsiveness

--- src/components/molecules/list-item/index.tsx
+++ src/components/molecules/list-item/index.tsx
@@ -1,11 +1,13 @@
 import "./styles.css";
 
-import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { ChangeEvent, useCallback, useState } from "react";
 
+import Button from "../../atoms/button";
 import CurrencyInput from "../../atoms/currency-input";
+import FlexContainer from "../../atoms/flex-container";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Product } from "../../../types/product";
-import Spacer from "../../atoms/spacer";
+import TextInput from "../../atoms/text-input";
 import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 
 type ListItemProps = {
@@ -16,82 +18,102 @@ type ListItemProps = {
 
 export default function ListItem({ item, removeItem, updateItem }: ListItemProps) {
   const [isEdit, setIsEdit] = useState(false);
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [currentItem, setCurrentItem] = useState<Product>({
-    checked: false,
-    productName: "Loading...",
-    id: "0",
-    price: "0",
-  });
+  const [valueInput, setValueInput] = useState(item.price);
+  const [nameInput, setNameInput] = useState(item.productName);
 
-  function handleDone(event: ChangeEvent<HTMLInputElement>) {
-    updateItem({ ...currentItem, checked: event.target.checked });
-  }
+  const handleCheck = useCallback(
+    (event: ChangeEvent<HTMLInputElement>, currentItem: Product) => {
+      const updatedItem = { ...currentItem, checked: event.target.checked };
+      updateItem(updatedItem);
+    },
+    [updateItem]
+  );
 
-  function handleProductName(event: ChangeEvent<HTMLInputElement>) {
-    setCurrentItem((prev) => ({ ...prev, productName: event.target.value }));
-  }
+  const handleDelete = useCallback(
+    (itemToBeDelete: typeof item) => {
+      removeItem(itemToBeDelete);
+    },
+    [removeItem]
+  );
 
-  // function handleProductPrice(event: ChangeEvent<HTMLInputElement>) {
-  //   setCurrentItem((prev) => ({ ...prev, price: event.target.value }));
-  // }
+  const handleEdit = useCallback(
+    (itemToBeEdited: typeof item) => {
+      if (!isEdit) {
+        setIsEdit(true);
+        return;
+      }
 
-  function handleEdit() {
-    setIsEdit((prev) => !prev);
+      const updatedItem = {
+        ...itemToBeEdited,
+        productName: nameInput,
+        price: valueInput,
+        checked: itemToBeEdited.checked,
+      };
 
-    if (isEdit && !currentItem.productName) return;
+      updateItem(updatedItem);
 
-    if (isEdit) handleSave();
-
-    setTimeout(() => inputRef.current!.focus(), 100);
-  }
-
-  function handleSave() {
-    updateItem(currentItem);
-  }
-
-  useEffect(() => {
-    setCurrentItem(item);
-  }, [item]);
+      setIsEdit(false);
+    },
+    [isEdit, nameInput, valueInput, updateItem]
+  );
 
   return (
-    <li className="listitem__wrapper">
-      <div className="listitem__description__wrapper">
-        <div className="listitem__column">
-          <div className="listitem__row">
-            <input
-              type="checkbox"
-              id={currentItem.id?.toString()}
-              onChange={handleDone}
-              checked={currentItem.checked}
-            />
-            <label htmlFor={currentItem.id?.toString()}>
-              <input
-                value={currentItem.productName ?? ""}
-                onChange={handleProductName}
-                className={
-                  currentItem.checked
-                    ? "listitem__description-input--done listitem__description-input"
-                    : "listitem__description-input"
-                }
-                disabled={!isEdit}
-                ref={inputRef}
-              />
-            </label>
-          </div>
-          <Spacer orientation="vertical" size="8px" />
-          <CurrencyInput />
-        </div>
-      </div>
-
-      <div className="listitem__buttons__wrapper">
-        <button onClick={handleEdit} className="listitem__editbutton">
-          {isEdit ? "Salvar" : "Editar"}
-        </button>
-        <button onClick={() => removeItem(currentItem)} className="listitem__removebutton">
-          <FontAwesomeIcon icon={faTrashCan} />
-        </button>
-      </div>
-    </li>
+    <FlexContainer
+      borderRadius={{ small: "4px", medium: "4px" }}
+      padding={{ small: "16px", medium: "24px" }}
+      gap={{ small: "8px", medium: "16px" }}
+      flexDirection={{ small: "column", medium: "row" }}
+      backgroundColor={item.checked ? "#f1f8ff" : "#fff7f7"}
+      alignItems={{ small: "center", medium: "center" }}>
+      <FlexContainer
+        visible={{ small: false, medium: true }}
+        width={{ small: "100%", medium: "auto" }}
+        flexGrow={{ small: 1, medium: 1 }}
+        flexDirection={{ small: "column", medium: "row" }}
+        alignItems={{ small: "center", medium: "center" }}
+        justifyContent={{ small: "center", medium: "flex-start" }}>
+        <input
+          type="checkbox"
+          id={item.id}
+          onChange={(event) => handleCheck(event, item)}
+          checked={item.checked}
+          style={{ width: "24px", height: "24px", padding: "0", margin: "0" }}
+        />
+      </FlexContainer>
+      <FlexContainer
+        flexDirection={{ small: "row", medium: "row" }}
+        flexGrow={{ small: 5, medium: 5 }}
+        gap={{ small: "8px", medium: "16px" }}
+        width={{ small: "100%", medium: "100%" }}>
+        <TextInput
+          value={nameInput}
+          onChange={(event) => setNameInput(event.currentTarget.value)}
+          disabled={!isEdit}
+          width="100%"
+        />
+        <CurrencyInput value={valueInput} setValue={setValueInput} disabled={!isEdit} width="100%" />
+      </FlexContainer>
+      <FlexContainer
+        flexGrow={{ small: 1 }}
+        flexDirection={{ small: "row" }}
+        gap={{ small: "8px" }}
+        justifyContent={{ small: "flex-start", medium: "flex-end" }}>
+        <FlexContainer visible={{ small: true, medium: false }} flexGrow={{ small: 1 }}>
+          <input
+            type="checkbox"
+            id={item.id}
+            onChange={(event) => handleCheck(event, item)}
+            checked={item.checked}
+            style={{ width: "24px", height: "24px", padding: "0", margin: "0" }}
+          />
+        </FlexContainer>
+        <FlexContainer flexGrow={{ small: 3, medium: 1 }} gap={{ small: "8px", medium: "16px" }}>
+          <Button text={isEdit ? "Salvar" : "Editar"} onClick={() => handleEdit(item)}></Button>
+          <Button size="48px" variant="danger" onClick={() => handleDelete(item)}>
+            <FontAwesomeIcon icon={faTrashCan} />
+          </Button>
+        </FlexContainer>
+      </FlexContainer>
+    </FlexContainer>
   );
 }
