ea0d8466217a2ca776f51188f8eaa04bd2b4e4c4
Daniel <danieltostes@live.com>
2025-06-24 00:12:28 -0300

    chore: remove unused import

--- src/components/molecules/list-item/index.tsx
+++ src/components/molecules/list-item/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useEffect, useState } from "react";
+import { useCallback, useContext, useState } from "react";
 
 import Button from "../../atoms/button";
 import FlexContainer from "../../atoms/flex-container";
